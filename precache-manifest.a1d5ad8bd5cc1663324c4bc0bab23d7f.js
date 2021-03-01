f080185607209ccf4949ab8be0771785ee2cffef
MitchellZilberg <64373649+MitchellZilberg@users.noreply.github.com>
2021-03-01 12:42:38 -0500

    Updates

---
+++ precache-manifest.a1d5ad8bd5cc1663324c4bc0bab23d7f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5d63e918da0e67824f485deb4a6b572",
+    "url": "/mitchellzilberg/Note-Keeper-App.git/index.html"
+  },
+  {
+    "revision": "8736dab20406e13d2369",
+    "url": "/mitchellzilberg/Note-Keeper-App.git/static/js/2.39a27fbc.chunk.js"
+  },
+  {
+    "revision": "5ae27a8a9c6998cf4b86",
+    "url": "/mitchellzilberg/Note-Keeper-App.git/static/js/main.c8c67a99.chunk.js"
+  },
+  {
+    "revision": "25297ac4a73c4c7d2503",
+    "url": "/mitchellzilberg/Note-Keeper-App.git/static/js/runtime-main.86e28f2f.js"
+  }
+]);
